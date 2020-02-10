6fc988f4550f8a82f351ea2542e61c15cb0431b2
Maria Patrie <mariapatrie@gmail.com>
2020-02-10 21:05:12 +0300

    проверка минификации

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task("css", function () {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([ autoprefixer() ]))
-    /*.pipe(csso())
+    .pipe(csso())
     /*.pipe(postcss([ autoprefixer(), csso() ]))*/
     .pipe(rename("style.min.css"))
     .pipe(sourcemap.write("."))
