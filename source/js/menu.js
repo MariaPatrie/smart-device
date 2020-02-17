33b63bd68d4fdd2dc626deb82388c5d695b64e22
Maria Patrie <mariapatrie@gmail.com>
2020-02-17 22:39:56 +0300

    добавляет js-функционал

--- source/js/menu.js
+++ source/js/menu.js
@@ -33,22 +33,22 @@ navToggleContacts.addEventListener('click', function() {
   }
 });
 
-var link = document.querySelector(".feedback-button");
+var link = document.querySelector('.feedback-button');
 
-var popup = document.querySelector(".modal-feedback");
-var overlay = document.querySelector(".overlay");
-var close = popup.querySelector(".modal-close");
+var popup = document.querySelector('.modal-feedback');
+var overlay = document.querySelector('.overlay');
+var close = popup.querySelector('.modal-close');
 
-var form = popup.querySelector("form");
-var name = popup.querySelector("[name=name]");
-var phone = popup.querySelector("[name=phone]");
-var question = popup.querySelector("[name=question]");
+var form = popup.querySelector('form');
+var user = popup.querySelector('input[name="name"]');
+var phone = popup.querySelector('input[name="phone"]');
+var question = popup.querySelector('textarea[name="question"]');
 
 var isStorageSupport = true;
 var storage = "";
 
 try {
-  storage = localStorage.getItem("name");
+  storage = localStorage.getItem("user");
 } catch (err) {
   isStorageSupport = false;
 }
@@ -60,11 +60,12 @@ link.addEventListener("click", function (evt) {
   popup.classList.add("modal-show");
 
   if (storage) {
-    name.value = storage;
-    phone.value = storage;
-    question.focus();
+    user.value = storage;
+    //phone.value = localStorage.getItem("phone");
+    question.value = localStorage.getItem("question");
+    user.focus();
   } else {
-    name.focus();
+    user.focus();
   }
 });
 
@@ -75,16 +76,24 @@ close.addEventListener("click", function (evt) {
   popup.classList.remove("modal-error");
 });
 
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  overlay.classList.remove("overlay-show");
+  popup.classList.remove("modal-error");
+});
+
 form.addEventListener("submit", function (evt) {
-  if (!name.value || !phone.value) {
+  if (!user.value || !phone.value) {
     evt.preventDefault();
     popup.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
     popup.classList.add("modal-error");
   } else {
     if (isStorageSupport) {
-      localStorage.setItem("name", name.value);
+      localStorage.setItem("user", user.value);
       localStorage.setItem("phone", phone.value);
+      localStorage.setItem("question", question.value);
     }
   }
 });
@@ -99,3 +108,51 @@ window.addEventListener("keydown", function (evt) {
     }
   }
 });
+
+function setCursorPosition(pos, elem) {
+  elem.focus();
+  if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+  else if (elem.createTextRange) {
+    var range = elem.createTextRange();
+    range.collapse(true);
+    range.moveEnd("character", pos);
+    range.moveStart("character", pos);
+    range.select()
+  }
+};
+
+function mask(evt) {
+  var matrix = "+7 (___) ___ ____";
+  var i = 0;
+  var def = matrix.replace(/\D/g, "");
+  var val = this.value.replace(/\D/g, "");
+
+  if (def.length >= val.length) val = def;
+  this.value = matrix.replace(/./g, function(a) {
+    return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+  });
+
+  if (evt.type == "blur") {
+    if (this.value.length == 2) this.value = ""
+  } else setCursorPosition(this.value.length, this)
+};
+
+phone.addEventListener("input", mask);
+phone.addEventListener("focus", mask);
+phone.addEventListener("blur", mask);
+
+function scrollTo(evt) {
+  evt.preventDefault();
+  var blockID = this.getAttribute('href');
+
+  document.querySelector(blockID).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+};
+
+var anchorFeedback = document.querySelector('.scroll-to-feedback');
+var anchorAdvantages = document.querySelector('.scroll-to-advantages');
+
+anchorFeedback.addEventListener("click", scrollTo);
+anchorAdvantages.addEventListener("click", scrollTo);
